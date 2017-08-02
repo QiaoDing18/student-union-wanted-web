67dab3b4dacb07b37975d83bcae2003d182d0ab8
QiaoDing18 <tjoe18@126.com>
2017-08-02 17:00:27 +0800

    Update Join.vue

--- src/components/Join/Join.vue
+++ src/components/Join/Join.vue
@@ -1,87 +1,53 @@
 <template>
   <div id="join">
-    <div class="join-1">
-      <input type="text" id="name">
-      <select id="class">
-        <option value=""></option>
-        <option value="">软件1701</option>
-        <option value="">软件1702</option>
-        <option value="">软件1703</option>
-        <option value="">软件1704</option>
-        <option value="">软件1705</option>
-        <option value="">软件1706</option>
-        <option value="">软件1707</option>
-        <option value="">网络1701</option>
-        <option value="">网络1702</option>
-        <option value="">网络1703</option>
-        <option value="">网络1704</option>
-        <option value="">网络1705</option>
-        <option value="">计科1701</option>
-        <option value="">计科1702</option>
-        <option value="">计科1703</option>
-        <option value="">计科1704</option>
-        <option value="">计科1705</option>
-        <option value="">计科1706</option>
-        <option value="">计科1707</option>
+    <div class="join-1" id="test">
+      <input type="text" id="name" v-model="name">
+      <select id="class" v-model="iClass">
+        <option v-for="option in options">
+        {{ option.text }}
+        </option>
       </select>
-      <input type="text" id="num">
-      <input type="text" id="tel">
+      <input type="text" id="num" v-model="num">
+      <input type="text" id="tel" v-model="tel">
     </div>
 
     <div class="join-2">
-      <select id="first">
-        <option value=""></option>
-        <option value="">办公室</option>
-        <option value="">宣传部</option>
-        <option value="">体育部</option>
-        <option value="">文艺部</option>
-        <option value="">组织部</option>
-        <option value="">青志</option>
-        <option value="">外联部</option>
-        <option value="">学习部</option>
+      <select id="first" v-model="first">
+        <option v-for="group in groups">
+          {{ group.text }}
+        </option>
       </select>
-      <select id="second">
-        <option value=""></option>
-        <option value="">办公室</option>
-        <option value="">宣传部</option>
-        <option value="">体育部</option>
-        <option value="">文艺部</option>
-        <option value="">组织部</option>
-        <option value="">青志</option>
-        <option value="">外联部</option>
-        <option value="">学习部</option>
+      <select id="second" v-model="second">
+        <option v-for="group in groups">
+          {{ group.text }}
+        </option>
       </select>
-      <select id="third">
-        <option value=""></option>
-        <option value="">办公室</option>
-        <option value="">宣传部</option>
-        <option value="">体育部</option>
-        <option value="">文艺部</option>
-        <option value="">组织部</option>
-        <option value="">青志</option>
-        <option value="">外联部</option>
-        <option value="">学习部</option>
+      <select id="third" v-model="third">
+        <option v-for="group in groups">
+          {{ group.text }}
+        </option>
       </select>
-      <select id="whether">
-        <option value=""></option>
-        <option value="">&nbsp;&nbsp;&nbsp;是</option>
-        <option value="">&nbsp;&nbsp;&nbsp;否</option>
+      <select id="whether" v-model="whether">
+        <option v-for="whether in whethers">
+          {{ whether.text }}
+        </option>
       </select>
-    </div>
-
-    <img id="btn" src="./../../assets/join/join4.png" alt="">
-
-    <div class="join-3">
-      <textarea id="message" cols="30" rows="10"></textarea>
     </div>
     
-    <div id="back" style="display:none;"></div>
-    <div id="table" style="display:none;">
-      <input id="numInput" type="text" readonly="readonly">
-      <input id="password" type="password" placeholder="教务系统密码">
-      <input id="verCode" type="text" placeholder="请输入验证码">
-      <img id="verPic" alt="">
-      <img id="btnGo" src="./../../assets/login/btn.png" alt="">
+    <!-- 登录窗按钮 -->
+    <img id="btn" v-on:click="showTable()" src="./../../assets/join/join4.png" alt="">
+
+    <div class="join-3">
+      <textarea id="message" cols="30" rows="10" v-model="message"></textarea>
+    </div>
+    
+    <div v-show="ok" id="back" v-on:click="closeTable()"></div>
+    <div v-show="ok" id="table">
+      <input id="numInput" type="text" readonly="readonly" v-bind:value=num>
+      <input id="password" type="password" placeholder="教务系统密码" v-model="password">
+      <input id="verCode" type="text" placeholder="请输入验证码" v-model="verCode">
+      <img id="verPic" v-on:click="getVer()" v-bind:src=verBase alt="">
+      <img id="btnGo" v-on:click="Add()" src="./../../assets/login/btn.png" alt="">
     </div>
   </div>
 </template>
